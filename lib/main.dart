758053fbc100add4c08a39f2f1b141f32a184af9
Monster-Mohamed <ma8494927@gmail.com>
2022-06-26 13:05:18 +0200

    Facebook Monster v0.1 * Added quotes * Row * Column ..more

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,40 +26,368 @@ class Facebook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[300],
-        centerTitle: true,
-        title: Text(
-          "Facebook",
-          style: TextStyle(color: Colors.white),
+        appBar: AppBar(
+          // app bar styles
+          backgroundColor: Colors.red[300],
+          centerTitle: true,
+          // to show the shadow
+          elevation: 10,
+          // app bar widgets
+          title: Text(
+            "Facebook",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          // the widget before title
+          leading: IconButton(
+            // the icon
+            icon: Icon(Icons.menu),
+            // when click on this icon
+            onPressed: () {
+              // clicked...
+            },
+          ),
+          // in the very right place
+          actions: <Widget>[
+            // the container is like a div in html
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Icon(
+                Icons.search,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 20, 0),
+              child: Icon(
+                Icons.chat,
+              ),
+            )
+          ],
         ),
-        leading: IconButton(
-          // the icon
-          icon: Icon(Icons.menu),
-          // when click on this icon
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red[400],
           onPressed: () {
-            // to show the toast message
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              // the content of this message
-              content: Text("Menu bar is opened"),
-            ));
+            //
           },
+          child: IconButton(
+            // the icon
+            icon: Icon(Icons.add),
+            // when click on this icon
+            onPressed: () {
+              // to show the toast message
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                // the content of this message
+                content: Text("A new quote added successfully!"),
+              ));
+            },
+          ),
         ),
-        actions: <Widget>[
-          Container(
-            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: Icon(
-              Icons.search,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // title
+                Text(
+                  "Monster Quotes",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                // body
+                Column(
+                  // all quotes are here
+                  children: [
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 20, 20, 0),
+                            padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
+                            decoration: BoxDecoration(
+                              color: Colors.red[300],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Text(
+                              "“Be yourself; everyone else is already taken.”",
+                              style: TextStyle(
+                                fontSize: 18,
+                                // overflow: TextOverflow.ellipsis,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                letterSpacing: 1.3,
+                              ),
+                              maxLines: 2,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 20, 20, 0),
+                            padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
+                            decoration: BoxDecoration(
+                              color: Colors.red[300],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Text(
+                              "“Be yourself; everyone else is already taken.”",
+                              style: TextStyle(
+                                fontSize: 18,
+                                // overflow: TextOverflow.ellipsis,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                letterSpacing: 1.3,
+                              ),
+                              maxLines: 2,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.fromLTRB(0, 20, 20, 0),
+                            padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
+                            decoration: BoxDecoration(
+                              color: Colors.red[300],
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Text(
+                              "“Be yourself; everyone else is already taken.”",
+                              style: TextStyle(
+                                fontSize: 18,
+                                // overflow: TextOverflow.ellipsis,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                                letterSpacing: 1.3,
+                              ),
+                              maxLines: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // to make spaces without use margins
+                    // SizedBox(height: 20),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.red[300],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        "“You only live once, but if you do it right, once is enough.”",
+                        style: TextStyle(
+                          fontSize: 18,
+                          // overflow: TextOverflow.ellipsis,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                          letterSpacing: 1.3,
+                        ),
+                        maxLines: 2,
+                      ),
+                    ),
+
+                    // ---------------------------
+                    // ---------------------------
+                    // Expanded Section
+                    // Widget
+
+                    Container(
+                      height: 300,
+                      margin: EdgeInsets.fromLTRB(0, 40, 0, 80),
+                      // for customizing the border
+                      decoration: BoxDecoration(
+                        color: Colors.pink[300],
+                        // border radius
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.indigo[300],
+                              height: 100,
+                            ),
+                          ),
+                          Expanded(
+                            // that's mean this widget will take 3 chunks of the empty space
+                            flex: 2,
+                            child: Container(
+                              color: Colors.blue[300],
+                              height: 100,
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.teal[300],
+                              height: 100,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+
+                    // End Widget
+                    // End Expanded Section
+                    // ---------------------------
+                    // ---------------------------
+                  ],
+                ),
+              ],
             ),
           ),
-          Container(
-            margin: EdgeInsets.fromLTRB(10, 0, 20, 0),
-            child: Icon(
-              Icons.chat,
-            ),
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
